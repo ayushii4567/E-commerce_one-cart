dadc1daf6c62ef76c99ef6ea9d9b3933e5c35799
Ayushi Gupta <ayushig948@gmail.com>
2026-05-23 15:10:25 +0530

    Update AuthContext.jsx

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://e-commerce-one-cart-backend.onrender.com";
 
   return (
     <authDataContext.Provider value={{ serverUrl }}>
